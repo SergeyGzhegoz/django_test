25370fdf655337d316a900aefdf21dac5501c329
SergeyGzhegoz <s.gz@yandex.ru>
2022-02-14 14:21:28 +0300

    Проверка. Push

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Проверка работа с гитом")
 
 
 # Press the green button in the gutter to run the script.
