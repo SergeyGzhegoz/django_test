9b7187a0c81c8d2879af920fcda10f325b1b53e3
Sergey <s.gz@yandex.ru>
2022-02-22 14:05:33 +0300

    Урок #10-11 - Динамически изменяемые страницы. Редактирование и удаление записей.

--- itproger/news/views.py
+++ itproger/news/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     news = Articles.objects.order_by('title')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'  # Переменная ('article') для передачи данных в шаблон.
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'   # Куда перейти после удаления.
+    template_name = 'news/news-delete.html'
+
 
 
 def create(request):
